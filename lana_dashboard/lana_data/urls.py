3df8e15485a480294b8eab34f6ea4347d9302e9c
Sebastian Brückner <mail@invlid.com>
2016-04-28 18:13:05 +0200

    Fixed URLs.

--- lana_dashboard/lana_data/urls.py
+++ lana_dashboard/lana_data/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from lana_dashboard.lana_data import views
 
 urlpatterns = [
-	url(r'^institutions/', views.list_institutions, name='institutions'),
-	url(r'^autonomous-systems/', views.list_autonomous_systems, name='autonomous_systems'),
-	url(r'^ipv4/', views.list_ipv4, name='ipv4'),
+	url(r'^institutions/$', views.list_institutions, name='institutions'),
+	url(r'^autonomous-systems/$', views.list_autonomous_systems, name='autonomous_systems'),
+	url(r'^ipv4/$', views.list_ipv4, name='ipv4'),
 ]
