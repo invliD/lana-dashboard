237a1021900a093dea468d7c645b0c2ff6bf047a
Sebastian Brückner <mail@invlid.com>
2016-04-28 18:03:42 +0200

    Added header highlighting.

--- lana_dashboard/lana_data/views.py
+++ lana_dashboard/lana_data/views.py
@@ -7,6 +7,7 @@ def list_institutions(request):
 	institutions = Institution.objects.all()
 
 	return render(request, 'institutions_list.html', {
+		'header_active': 'institutions',
 		'institutions': institutions,
 	})
 
@@ -15,6 +16,7 @@ def list_autonomous_systems(request):
 	autonomous_systems = AutonomousSystem.objects.all()
 
 	return render(request, 'autonomous_systems_list.html', {
+		'header_active': 'autonomous_systems',
 		'autonomous_systems': autonomous_systems,
 	})
 
@@ -23,5 +25,6 @@ def list_ipv4(request):
 	subnets = IPv4Subnet.objects.all()
 
 	return render(request, 'ipv4_list.html', {
+		'header_active': 'ipv4',
 		'subnets': subnets,
 	})
