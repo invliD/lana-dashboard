cfd6b5f6b3c20179baba3cf8b7667c4ed670ee32
Sebastian Brückner <mail@invlid.com>
2016-05-22 00:10:23 +0200

    Cleaned up URLs.

--- lana_dashboard/lana_data/urls.py
+++ lana_dashboard/lana_data/urls.py
@@ -8,15 +8,18 @@ urlpatterns = [
 	url(r'^institutions/(?P<code>.+)/tunnels/$', views.list_institution_tunnels, name='institution-tunnels'),
 	url(r'^institutions/(?P<code>.+)/$', views.show_institution, name='institution-details'),
 	url(r'^institutions/(?P<code>.+)/edit$', views.edit_institution, name='institution-edit'),
+
 	url(r'^autonomous-systems/$', views.list_autonomous_systems, name='autonomous_systems'),
 	url(r'^autonomous-systems/create$', views.edit_autonomous_system, name='autonomous_system-create'),
-	url(r'^autonomous-systems/(?P<as_number>.+)/$', views.show_autonomous_system, name='autonomous_system-details'),
-	url(r'^autonomous-systems/(?P<as_number>.+)/delete$', views.delete_autonomous_system, name='autonomous_system-delete'),
-	url(r'^autonomous-systems/(?P<as_number>.+)/edit$', views.edit_autonomous_system, name='autonomous_system-edit'),
+	url(r'^autonomous-systems/(?P<as_number>\d+)/$', views.show_autonomous_system, name='autonomous_system-details'),
+	url(r'^autonomous-systems/(?P<as_number>\d+)/delete$', views.delete_autonomous_system, name='autonomous_system-delete'),
+	url(r'^autonomous-systems/(?P<as_number>\d+)/edit$', views.edit_autonomous_system, name='autonomous_system-edit'),
+
 	url(r'^ipv4/$', views.list_ipv4, name='ipv4'),
 	url(r'^ipv4/create$', views.edit_ipv4, name='ipv4-create'),
 	url(r'^ipv4/(?P<network>.+)/$', views.show_ipv4, name='ipv4-details'),
 	url(r'^ipv4/(?P<network>.+)/edit$', views.edit_ipv4, name='ipv4-edit'),
+
 	url(r'^tunnels/$', views.list_tunnels, name='tunnels'),
 	url(r'^tunnels/create$', views.edit_tunnel, name='tunnel-create'),
 	url(r'^tunnels/create/form$', views.generate_tunnel_form, name='tunnel-create-form'),
@@ -24,5 +27,6 @@ urlpatterns = [
 	url(r'^tunnels/AS(?P<as_number1>\d+)-AS(?P<as_number2>\d+)/delete', views.delete_tunnel, name='tunnel-delete'),
 	url(r'^tunnels/AS(?P<as_number1>\d+)-AS(?P<as_number2>\d+)/edit$', views.edit_tunnel, name='tunnel-edit'),
 	url(r'^tunnels/AS(?P<as_number1>\d+)-AS(?P<as_number2>\d+)/autonomous_systems/$', views.list_tunnel_autonomous_systems, name='tunnel-autonomous_systems'),
+
 	url(r'^search/(.+)?$', views.search, name='search'),
 ]
