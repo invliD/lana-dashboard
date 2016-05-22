947804636859474d305ce8195562bdf38228ed50
Sebastian Brückner <mail@invlid.com>
2016-05-22 16:11:34 +0200

    Added VTun and Fastd tunnels and endpoints to admin.

--- lana_dashboard/lana_data/admin.py
+++ lana_dashboard/lana_data/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from lana_dashboard.lana_data.models import AutonomousSystem, Institution, IPv4Subnet, Tunnel, TunnelEndpoint
+from lana_dashboard.lana_data.models import (
+	AutonomousSystem,
+	FastdTunnel,
+	FastdTunnelEndpoint,
+	Institution,
+	IPv4Subnet,
+	Tunnel,
+	TunnelEndpoint,
+	VtunTunnel,
+	VtunTunnelEndpoint,
+)
 
 
 class InstitutionAdmin(admin.ModelAdmin):
@@ -19,12 +29,32 @@ class TunnelAdmin(admin.ModelAdmin):
 	list_display = ('comment', 'endpoint1', 'endpoint2')
 
 
+class FastdTunnelAdmin(TunnelAdmin):
+	pass
+
+
+class VtunTunnelAdmin(TunnelAdmin):
+	pass
+
+
 class TunnelEndpointAdmin(admin.ModelAdmin):
 	list_display = ('autonomous_system',)
+
+
+class FastdTunnelEndpointAdmin(TunnelEndpointAdmin):
+	list_display = ('autonomous_system', 'port')
+
+
+class VtunTunnelEndpointAdmin(TunnelEndpointAdmin):
+	list_display = ('autonomous_system', 'port')
 
 
 admin.site.register(Institution, InstitutionAdmin)
 admin.site.register(AutonomousSystem, AutonomousSystemAdmin)
 admin.site.register(IPv4Subnet, IPv4SubnetAdmin)
 admin.site.register(Tunnel, TunnelAdmin)
+admin.site.register(FastdTunnel, FastdTunnelAdmin)
+admin.site.register(VtunTunnel, VtunTunnelAdmin)
 admin.site.register(TunnelEndpoint, TunnelEndpointAdmin)
+admin.site.register(FastdTunnelEndpoint, FastdTunnelEndpointAdmin)
+admin.site.register(VtunTunnelEndpoint, VtunTunnelEndpointAdmin)
