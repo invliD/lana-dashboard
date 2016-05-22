aebf661a1b7aa42cc386bad9a54bf90521d32b72
Sebastian Brückner <mail@invlid.com>
2016-05-22 16:18:15 +0200

    Refactored admin form registration.

--- lana_dashboard/lana_data/admin.py
+++ lana_dashboard/lana_data/admin.py
@@ -1,60 +1,48 @@
 from django.contrib import admin
 
-from lana_dashboard.lana_data.models import (
-	AutonomousSystem,
-	FastdTunnel,
-	FastdTunnelEndpoint,
-	Institution,
-	IPv4Subnet,
-	Tunnel,
-	TunnelEndpoint,
-	VtunTunnel,
-	VtunTunnelEndpoint,
-)
+from lana_dashboard.lana_data import models
 
 
+@admin.register(models.Institution)
 class InstitutionAdmin(admin.ModelAdmin):
 	list_display = ('name', 'code')
 
 
+@admin.register(models.AutonomousSystem)
 class AutonomousSystemAdmin(admin.ModelAdmin):
 	list_display = ('as_number', 'fqdn', 'comment', 'institution')
 
 
+@admin.register(models.IPv4Subnet)
 class IPv4SubnetAdmin(admin.ModelAdmin):
 	list_display = ('network', 'dns_server', 'comment', 'institution')
 
 
+@admin.register(models.Tunnel)
 class TunnelAdmin(admin.ModelAdmin):
 	list_display = ('comment', 'endpoint1', 'endpoint2')
 
 
+@admin.register(models.FastdTunnel)
 class FastdTunnelAdmin(TunnelAdmin):
 	pass
 
 
+@admin.register(models.VtunTunnel)
 class VtunTunnelAdmin(TunnelAdmin):
 	pass
 
 
+@admin.register(models.TunnelEndpoint)
 class TunnelEndpointAdmin(admin.ModelAdmin):
 	list_display = ('autonomous_system',)
 
 
+@admin.register(models.FastdTunnelEndpoint)
 class FastdTunnelEndpointAdmin(TunnelEndpointAdmin):
 	list_display = ('autonomous_system', 'port')
 
 
+@admin.register(models.VtunTunnelEndpoint)
 class VtunTunnelEndpointAdmin(TunnelEndpointAdmin):
 	list_display = ('autonomous_system', 'port')
-
-
-admin.site.register(Institution, InstitutionAdmin)
-admin.site.register(AutonomousSystem, AutonomousSystemAdmin)
-admin.site.register(IPv4Subnet, IPv4SubnetAdmin)
-admin.site.register(Tunnel, TunnelAdmin)
-admin.site.register(FastdTunnel, FastdTunnelAdmin)
-admin.site.register(VtunTunnel, VtunTunnelAdmin)
-admin.site.register(TunnelEndpoint, TunnelEndpointAdmin)
-admin.site.register(FastdTunnelEndpoint, FastdTunnelEndpointAdmin)
-admin.site.register(VtunTunnelEndpoint, VtunTunnelEndpointAdmin)
